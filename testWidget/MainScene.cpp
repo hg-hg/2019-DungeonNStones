e98d1ad2ac15130baa2da8c27bd4643db93430f5
hg-hg <zyongtai@163.com>
2019-06-12 21:14:18 +0800

    modified shop and mainScene

--- testWidget/MainScene.cpp
+++ testWidget/MainScene.cpp
@@ -9,6 +9,27 @@ MainScene::MainScene(QWidget *parent)
 	logo->setFileName(":/word/Resources/word/Title_pic.gif");
 	ui.logoPic->setMovie(logo);
 	logo->start();
+
+	auto cha1 = new QMovie(this);
+	auto cha2 = new QMovie(this);
+	auto cha3 = new QMovie(this);
+	auto cha4 = new QMovie(this);
+	cha1->setScaledSize(ui.chaPic1->size());
+	cha2->setScaledSize(ui.chaPic2->size());
+	cha3->setScaledSize(ui.chaPic3->size());
+	cha4->setScaledSize(ui.chaPic4->size());
+	cha1->setFileName(":/skin/Resources/skin/big_demon_ro.gif");
+	cha2->setFileName(":/skin/Resources/skin/knight.gif");
+	cha3->setFileName(":/skin/Resources/skin/elf.gif");
+	cha4->setFileName(":/skin/Resources/skin/wizzard.gif");
+	ui.chaPic1->setMovie(cha1);
+	ui.chaPic2->setMovie(cha2);
+	ui.chaPic3->setMovie(cha3);
+	ui.chaPic4->setMovie(cha4);
+	cha1->start();
+	cha2->start();
+	cha3->start();
+	cha4->start();
 }
 
 MainScene::~MainScene()
